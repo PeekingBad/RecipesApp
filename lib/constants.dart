73271365b0798476dde411770e1fdb5a8342955a
Verwegen,Daniell,D <d.verwegen@student.fontys.nl>
2024-03-25 17:02:09 +0100

    build & style - build appbar + styling

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import "package:flutter/material.dart";
+
+const Color background = Color.fromARGB(255, 52, 144, 165);
---
+++ lib/screens/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../constants.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: background,
+        title: const Text("Home"),
+        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 24),
+      ),
+      body: Container(),
+    );
+  }
+}
